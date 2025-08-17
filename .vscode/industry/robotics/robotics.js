8ca6757c913e9062cb39aaf5b45a7e00c8fe88c3
Aman Sreejesh <98895109+AmanSreejesh@users.noreply.github.com>
2025-08-17 20:27:09 +0000

    added images and js

---
+++ .vscode/industry/robotics/robotics.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", () => {
+    let text = document.getElementById("text");
+    let subtext = document.getElementById("subtext");
+    let background = document.getElementById("parallax-1");
+    let earth = document.getElementById("parallax-2");
+    let robot = document.getElementById("parallax-3");
+
+    window.addEventListener("scroll", () => {
+        let value = window.scrollY;
+        text.style.marginTop = value * 0.5 + "px";
+        subtext.style.marginTop = value * -1.2 + "px";
+        earth.style.top = value * -0.05 + "px";
+        robot.style.top = value * -0.5 + "px";
+    });
+});
