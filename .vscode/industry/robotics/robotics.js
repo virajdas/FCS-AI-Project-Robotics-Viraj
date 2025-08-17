7b72161912e690389bc9e84814770e1144781430
Viraj Das <97415762+virajdas@users.noreply.github.com>
2025-08-17 19:28:09 +0000

    Fleshed out parallax effect on landing page

--- .vscode/industry/robotics/robotics.js
+++ .vscode/industry/robotics/robotics.js
@@ -5,7 +5,7 @@ let robot = document.getElementById("parallax-3");
 
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
-    text.style.marginTop = value * 2.5 + "px";
+    text.style.marginTop = value * 0.5 + "px";
     earth.style.top = value * -0.05 + "px";
-    robot.style.top = value * -0.05 + "px";
+    robot.style.top = value * -0.5 + "px";
 });
