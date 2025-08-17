0afa2c634740df9475619f4c17e9f4b53f8ca6e1
Viraj Das <97415762+virajdas@users.noreply.github.com>
2025-08-17 19:16:20 +0000

    Added basic parallax...NEEDS MORE WORK!

---
+++ .vscode/industry/robotics/robotics.js
@@ -0,0 +1,11 @@
+let text = document.getElementById("text");
+let background = document.getElementById("parallax-1");
+let earth = document.getElementById("parallax-2");
+let robot = document.getElementById("parallax-3");
+
+window.addEventListener("scroll", () => {
+    let value = window.scrollY;
+    text.style.marginTop = value * 2.5 + "px";
+    earth.style.top = value * -0.05 + "px";
+    robot.style.top = value * -0.05 + "px";
+});
